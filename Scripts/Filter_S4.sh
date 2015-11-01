d71b2d9ec7724a5a751a2bf44bba6bf53b774a43
Chao Zhang <czhmu2010@gmail.com>
2015-11-01 16:24:02 -0500

    Minor changes

--- Scripts/Filter_S4.sh
+++ Scripts/Filter_S4.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 source ./configs.sh
 echo "Step 4:"
-echo "Aligning the remaining reads to human sequence database with BLAST"
-blastn -db $BLASTHUMANDB -num_threads $CPU -num_descriptions 1 -num_alignments 1 -word_size 13 -evalue 0.0001 -query ${WORKDIR}/${SAMPLE}_Filter_S3.fasta -out ${WORKDIR}/${SAMPLE}_Filter_S4.out
+echo "Aligning the remaining reads to human sequence database with MegaBLAST"
+megablast -d $BLASTHUMANDB -a $CPU -W 16 -e 0.0000001 -v 1 -b 0 -i ${WORKDIR}/${SAMPLE}_Filter_S3.fasta -o ${WORKDIR}/${SAMPLE}_Filter_S4.out
 java -Xms2g -Xmx6g -jar WGSparser.jar parseBLAST ${WORKDIR}/${SAMPLE}_Filter_S3.fasta ${WORKDIR}/${SAMPLE}_Filter_S4.fasta
 
 echo "Removing intermediate files"
--- Scripts/Filter_S5.sh
+++ Scripts/Filter_S5.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 source ./configs.sh
 echo "Step 5:"
-echo "Aligning the remaining reads to human sequence database with MegaBLAST"
-megablast -d $BLASTHUMANDB -a $CPU -W 16 -e 0.0000001 -v 1 -b 0 -i ${WORKDIR}/${SAMPLE}_Filter_S4.fasta -o ${WORKDIR}/${SAMPLE}_Filter_S5.out
+echo "Aligning the remaining reads to human sequence database with BLAST"
+blastn -db $BLASTHUMANDB -num_threads $CPU -num_descriptions 1 -num_alignments 1 -word_size 13 -evalue 0.0001 -query ${WORKDIR}/${SAMPLE}_Filter_S4.fasta -out ${WORKDIR}/${SAMPLE}_Filter_S5.out
 java -Xms2g -Xmx6g -jar WGSparser.jar parseBLAST ${WORKDIR}/${SAMPLE}_Filter_S4.fasta ${WORKDIR}/${SAMPLE}_Filter_S5.fasta
 
 echo "Removing intermediate files"
